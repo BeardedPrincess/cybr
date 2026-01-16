62599c803760c03d4d0375bd181ea29358f123a7
Justin Hansen <justin@beardedprincess.com>
2026-01-16 15:15:58 -0700

    Fix hard-coded path in showConnections.sh

--- sshmanager/demo-authz/bin/showConnections.sh
+++ sshmanager/demo-authz/bin/showConnections.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+set -euo pipefail
 
-find /root/ubussh/vol-ussh-data/logs/ -name connections.log -exec cat {} \;
+SCRIPT_ROOT=${SCRIPT_ROOT:-$(dirname "$(realpath "${0}")")}
+
+scriptRoot=$(dirname "$(realpath "${0}")")
+source "$(dirname "$(realpath "${0}")")/lib/_lib.sh" 
+common_init
+
+find ${DEMO_ROOT}/vol-ussh-data/logs/ -name connections.log -exec cat {} \;
